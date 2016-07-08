27a01e4aff37995a66827ab48d0f2ec12fc222e4
Andrew-Heath <aheath42@gmail.com>
2016-07-08 15:42:38 -0700

    Links service can make GET and POST requests

--- client/app/services/services.js
+++ client/app/services/services.js
@@ -1,7 +1,36 @@
 angular.module('shortly.services', [])
 
 .factory('Links', function ($http) {
-  // Your code here
+  var getAll = function() {
+    // TODO: get all links
+    // GET request to '/api/links'
+    return $http({
+      method: 'GET',
+      url: '/api/links'
+    })
+    .then(function(resp) {
+      // Pass info to links controller
+      return resp.data;
+    });
+  };
+
+  var addOne = function(linkToAdd) {
+    // TODO: make a new link
+    // POST request to '/api/links'
+    return $http({
+      method: 'POST',
+      url: '/api/links',
+      data: linkToAdd
+    })
+    .then(function(resp) {
+      // return server response
+      return resp;
+    });
+  };
+  return {
+    getAll: getAll,
+    addOne: addOne
+  };
 })
 .factory('Auth', function ($http, $location, $window) {
   // Don't touch this Auth service!!!
@@ -11,33 +40,33 @@ angular.module('shortly.services', [])
   // that JWT is then stored in localStorage as 'com.shortly'
   // after you signin/signup open devtools, click resources,
   // then localStorage and you'll see your token from the server
-  var signin = function (user) {
+  var signin = function(user) {
     return $http({
       method: 'POST',
       url: '/api/users/signin',
       data: user
     })
-    .then(function (resp) {
+    .then(function(resp) {
       return resp.data.token;
     });
   };
 
-  var signup = function (user) {
+  var signup = function(user) {
     return $http({
       method: 'POST',
       url: '/api/users/signup',
       data: user
     })
-    .then(function (resp) {
+    .then(function(resp) {
       return resp.data.token;
     });
   };
 
-  var isAuth = function () {
+  var isAuth = function() {
     return !!$window.localStorage.getItem('com.shortly');
   };
 
-  var signout = function () {
+  var signout = function() {
     $window.localStorage.removeItem('com.shortly');
     $location.path('/signin');
   };
