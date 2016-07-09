ec85b0c9f6ee2920b9aadcc2f3ad71caaee8af77
Andrew-Heath <aheath42@gmail.com>
2016-07-09 10:45:13 -0700

    Add authorization condition to links and shorten

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -1,13 +1,19 @@
 angular.module('shortly.links', [])
 
-.controller('LinksController', function ($scope, Links) {
+.controller('LinksController', function ($scope, Links, Auth) {
   $scope.data = { links: [] };
 
   var getAllLinks = function() {
     Links.getAll().then(function(links) {
       $scope.data.links = links;
-    });
+    });   
   };
 
-  getAllLinks();
+  if (Auth.isAuth()) {
+    getAllLinks();
+  } else {  
+    console.log('Not authorized');
+    // redirect to login?
+    Auth.signout();
+  }
 });
--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -1,10 +1,16 @@
 angular.module('shortly.shorten', [])
 
-.controller('ShortenController', function ($scope, $location, Links) {
+.controller('ShortenController', function ($scope, $location, Links, Auth) {
   $scope.link = {}; // Maybe remove?
 
   $scope.addLink = function() {
     Links.addOne($scope.newLink);
     $scope.newLink = '';
   };
+
+  if (!Auth.isAuth()) {  
+    console.log('Not authorized');
+    // redirect to login?
+    Auth.signout();
+  }
 });
